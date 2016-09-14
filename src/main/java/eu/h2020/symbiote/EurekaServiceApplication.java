403a7a70b5cd699941fdd8e87fffce872cd2391a
Mateusz Łukaszenko <mateuszl@man.poznan.pl>
2016-09-14 10:45:30 +0200

    initial commit - working project

---
+++ src/main/java/eu/h2020/symbiote/EurekaServiceApplication.java
@@ -0,0 +1,18 @@
+package eu.h2020.symbiote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by mateuszl on 14.08.2016.
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+}
